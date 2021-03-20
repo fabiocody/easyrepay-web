fbb908d9fcdb47eadb75093d7ec02a4fb740cc75
Fabio Codiglioni <fabiocody@icloud.com>
2021-03-20 11:04:03 +0100

    Update release.sh and apiUrl for production

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://easyrepay-be.herokuapp.com'
+  apiUrl: 'https://easyrepay.herokuapp.com'
 };
--- release.sh
+++ release.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 git checkout release
+git merge main
 cd angular
 npm run build
 cd ..
+git add .
 git commit -am "Release"
 git push
+git checkout main
