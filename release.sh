4f1ff0b2670ac1012b878a57deac893270eae254
Fabio Codiglioni <fabiocody@icloud.com>
2021-04-02 00:00:20 +0200

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-git checkout release &&
-git merge main &&
-cd angular &&
-npm install &&
-npm run build &&
-cd .. &&
-npm install &&
-npm run build &&
-git add . &&
-git commit -am "Release" &&
-git push &&
+git checkout release
+git merge main
+cd angular
+npm install
+npm run build
+cd ..
+npm install
+npm run build
+git add .
+git commit -am "Release"
+git push
 git checkout main
