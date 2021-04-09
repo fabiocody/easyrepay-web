f01db4ad6018f217515621beca3ea8dd79c379cd
Fabio Codiglioni <fabiocody@icloud.com>
2021-04-09 12:15:35 +0200

    Optimizations
    - All API calls return promises instead of observables
    - Add subsink to unsubscribe from observables

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,21 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {UserService} from './services/user.service';
 import {Router} from '@angular/router';
+import {SubSink} from 'subsink';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy{
     public title = 'EasyRepay';
     public loading = false;
+    private subs = new SubSink();
 
     constructor(
         private router: Router,
         private userService: UserService,
     ) {
-        this.userService.user.subscribe(user => {
+        this.subs.sink = this.userService.user.subscribe(user => {
             if (user) {
                 if (this.loading) {
                     this.router.navigate(['/people']).then();
@@ -28,5 +30,9 @@ export class AppComponent {
                 this.loading = true;
             }
         });
+    }
+
+    ngOnDestroy(): void {
+        this.subs.unsubscribe();
     }
 }
--- angular/src/app/services/user.service.ts
+++ angular/src/app/services/user.service.ts
@@ -1,31 +1,39 @@
-import {Injectable} from '@angular/core';
+import {Injectable, OnDestroy} from '@angular/core';
 import {Router} from '@angular/router';
 import {ApiService, LoginStatus} from './api.service';
 import {BehaviorSubject} from 'rxjs';
 import {UserDto} from '../../../../src/model/dto/user.dto';
+import {SubSink} from 'subsink';
 
 @Injectable({
     providedIn: 'root'
 })
-export class UserService {
+export class UserService implements OnDestroy {
     private userSubject = new BehaviorSubject<UserDto | null | undefined>(undefined);
     public user = this.userSubject.asObservable();
+    private subs = new SubSink();
 
     constructor(
         private router: Router,
         private apiService: ApiService,
     ) {
-        this.apiService.loginStatus.subscribe(status => {
+        this.subs.sink = this.apiService.loginStatus.subscribe(status => {
             switch (status) {
                 case LoginStatus.LOGGED_IN:
-                    this.apiService.getUserInfo().subscribe(user => this.userSubject.next(user));
+                    this.apiService.getUserInfo()
+                        .then(user => this.userSubject.next(user))
+                        .catch(error => {
+                            console.error(error);
+                            this.userSubject.next(null);
+                        });
                     break;
                 case LoginStatus.LOGGED_OUT:
                     this.userSubject.next(null);
                     break;
                 case LoginStatus.TOKEN_EXPIRED:
-                    this.apiService.refreshToken().subscribe({
-                        error: () => this.userSubject.next(null),
+                    this.apiService.refreshToken().catch(error => {
+                        console.error(error);
+                        this.userSubject.next(null);
                     });
                     break;
                 default:
@@ -34,19 +42,20 @@ export class UserService {
         });
     }
 
-    public login(username: string, password: string): void {
-        this.apiService.login(username, password).subscribe(() => {
-            this.apiService.getUserInfo().subscribe(user => {
-                this.userSubject.next(user);
-                this.router.navigate(['/people']).then();
-            }, error => {
-                console.error(error);
-                this.userSubject.next(null);
-            });
-        }, error => {
-            console.error(error);
+    ngOnDestroy(): void {
+        this.subs.unsubscribe();
+    }
+
+    public async login(username: string, password: string): Promise<void> {
+        try {
+            await this.apiService.login(username, password);
+            const user = await this.apiService.getUserInfo();
+            this.userSubject.next(user);
+            this.router.navigate(['/people']).then();
+        } catch (e) {
+            console.error(e);
             this.userSubject.next(null);
-        });
+        }
     }
 
     public logout(): void {
