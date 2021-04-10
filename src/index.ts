5e10e22c1acfa32b290dd64bc7f5fa5e082b8367
Fabio Codiglioni <fabiocody@icloud.com>
2021-04-10 15:50:17 +0200

    Move api directory outside of src

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ app.use(morgan('[:method] :url (:status) - :res[content-length] B - :response-ti
 app.use(cors());
 app.use(helmet());
 app.use(rateLimit({windowMs: 1000, max: 300}));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(YAML.load(path.join(__dirname, 'api/swagger.yaml'))));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(YAML.load(path.join(__dirname, '../../api/swagger.yaml'))));
 
 /* SETUP SERVER WITH API ROUTES AND AUTHENTICATION */
 useExpressServer(app, {
