21b234e5723e9905a1016ae7f58c6aed8efb2d95
Fabio Codiglioni <fabiocody@icloud.com>
2021-05-08 11:34:22 +0200

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
+echo "-> Merging main into release"
 git checkout release
 rm -rf dist
 git merge main
+echo "-> Building frontend"
 cd angular
 npm install
 npm run build
+echo "-> Building backend"
 cd ..
 npm install
 npm run build
+echo "-> Committing release"
 git add .
 git commit -am "Release"
 git push
