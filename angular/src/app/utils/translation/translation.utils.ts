1033867e9cb6a7eb88494687a42739186d043736
Fabio Codiglioni <fabiocody@icloud.com>
2021-05-23 17:56:22 +0200

    issue-109
    - Modularize spinner

--- angular/src/app/utils/translation/translation.utils.ts
+++ angular/src/app/utils/translation/translation.utils.ts
@@ -1,8 +1,7 @@
-import {Injectable} from '@angular/core';
 import {NativeDateAdapter} from '@angular/material/core';
 import {TranslationService} from './translation.service';
 
-@Injectable()
+
 export class AppDateAdapter extends NativeDateAdapter {
     getFirstDayOfWeek(): number {
         return 1;
