7c3fee0a1bf4a6f40afc0a70028ff1d57570a835
Fabio Codiglioni <fabiocody@icloud.com>
2021-05-23 17:57:18 +0200

    issue-109
    - Make AppDateAdapter injectable

--- angular/src/app/utils/translation/translation.utils.ts
+++ angular/src/app/utils/translation/translation.utils.ts
@@ -1,7 +1,9 @@
+import {Injectable} from '@angular/core';
 import {NativeDateAdapter} from '@angular/material/core';
 import {TranslationService} from './translation.service';
 
 
+@Injectable()
 export class AppDateAdapter extends NativeDateAdapter {
     getFirstDayOfWeek(): number {
         return 1;
