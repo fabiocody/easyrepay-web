195bdf9614ba9065072db0d298f591dc6309c844
Fabio Codiglioni <fabiocody@icloud.com>
2021-05-23 21:25:36 +0200

    issue-109
    - Fix first DOW in date picker

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,8 +6,6 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {RequestInterceptorService} from './utils/request-interceptor/request-interceptor.service';
-import {DateAdapter} from '@angular/material/core';
-import {AppDateAdapter} from './utils/translation/translation.utils';
 import {NavbarModule} from './navbar/navbar.module';
 import {SpinnerModule} from './utils/spinner/spinner.module';
 
@@ -29,10 +27,6 @@ import {SpinnerModule} from './utils/spinner/spinner.module';
             provide: HTTP_INTERCEPTORS,
             useClass: RequestInterceptorService,
             multi: true
-        },
-        {
-            provide: DateAdapter,
-            useClass: AppDateAdapter
         },
     ],
     bootstrap: [AppComponent]
