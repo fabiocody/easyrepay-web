a71c697376796d452f23494c66bf8ed09508a55e
Fabio Codiglioni <fabiocody@icloud.com>
2021-07-31 12:26:35 +0200

    Add fake loading spinner to TransactionPage to improve UX

--- angular/src/app/transactions/transaction-page/transaction-page.component.ts
+++ angular/src/app/transactions/transaction-page/transaction-page.component.ts
@@ -12,11 +12,14 @@ export class TransactionPageComponent {
     public transaction: TransactionDto | null;
     public personId: number;
     public showDelete: boolean;
+    public loading = true;
     public error: string | null = null;
 
     constructor(private router: Router, public location: Location) {
         this.transaction = this.router.getCurrentNavigation()!.extras!.state!.transaction;
         this.personId = this.router.getCurrentNavigation()!.extras!.state!.personId;
         this.showDelete = !!this.transaction;
+        const timeout = 75 + Math.random() * 50; // Random between 75 and 125
+        setTimeout(() => (this.loading = false), timeout);
     }
 }
