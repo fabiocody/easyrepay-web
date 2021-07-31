a0127943fc8d49d6c7d12cc329e36200f1d211cf
Fabio Codiglioni <fabiocody@icloud.com>
2021-07-31 18:29:01 +0200

    Add environment variables for first user creation

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,19 +1,29 @@
 import {Injectable, Logger, OnModuleInit, UnauthorizedException} from '@nestjs/common';
 import {UserEntity} from '../model/entities/user.entity';
 import {hash as bcryptHash, compare as bcryptCompare} from 'bcrypt';
+import {ConfigService} from '@nestjs/config';
 
 @Injectable()
 export class UserService implements OnModuleInit {
     private logger = new Logger('UserService');
 
+    constructor(private configService: ConfigService) {}
+
     public async onModuleInit(): Promise<void> {
-        this.logger.debug('Initializing users');
-        const users = await UserEntity.find();
-        if (users.length === 0) {
-            this.logger.debug('Creating first user');
-            this.save('admin', 'admin', 'Admin').then();
+        const username = this.configService.get<string>('INIT_USER_USERNAME');
+        const password = this.configService.get<string>('INIT_USER_PASSWORD');
+        const name = this.configService.get<string>('INIT_USER_NAME');
+        if (username && password && name) {
+            this.logger.debug('Initializing users');
+            const users = await UserEntity.find();
+            if (users.length === 0) {
+                this.logger.debug('Creating first user');
+                this.save(username, password, name).then();
+            } else {
+                this.logger.debug('Skipping first user creation: user found');
+            }
         } else {
-            this.logger.debug('Skipping first user creation');
+            this.logger.debug('Skipping first user creation: environment variables not found');
         }
     }
 
